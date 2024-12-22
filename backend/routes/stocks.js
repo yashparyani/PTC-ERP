c362cd0b8882aeccb195454a1bf1e4e4f1b9296f
mparyanicodes <muskanparyani.codes@gmail.com>
2024-12-22 14:39:15 +0530

    Update stocks.js

--- backend/routes/stocks.js
+++ backend/routes/stocks.js
@@ -1,5 +1,5 @@
 // const multer  = require('multer')
-// const express = require('express');
+const express = require('express');
 const router = express.Router();
 
 // const path = require("path");
