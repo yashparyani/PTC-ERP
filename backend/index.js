120089cea62add6eb352a49e516c934b06e8f156
mparyanicodes <muskanparyani.codes@gmail.com>
2024-12-22 12:54:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const cors = require('cors');
 app.use(cors({
-  origin: 'https://ptc-erp-apis.vercel.app',
+  origin: 'http://localhost:3000/',
   credentials: true, 
 }));
 const cookieParser = require('cookie-parser');
